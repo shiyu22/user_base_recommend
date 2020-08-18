93f400f27fc9ed9c22219c5a95638ac81ffdefc1
shiyu22 <cshiyu22@gmail.com>
2020-08-18 18:51:24 +0800

    update

--- webserver/src/main.py
+++ webserver/src/main.py
@@ -55,4 +55,4 @@ def do_search_images_api(search_id: int, table_name: str=None):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app=app, host='127.0.0.1', port=8000)
+    uvicorn.run(app=app, host='192.168.1.85', port=8000)
